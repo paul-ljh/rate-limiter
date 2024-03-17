2b7b1e8e19aedb77b0dfb132ffecdce7f8595463
Paul Li <paul.ljh@icloud.com>
2024-03-16 21:05:31 -0400

    load config

--- main.go
+++ main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Route struct {
+	Endpoint  string `json:"endpoint"`
+	Burst     int    `json:"burst"`
+	Sustained int    `json:"sustained"`
+}
+
+func main() {
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully opened config.json")
+	defer configFile.Close()
+
+	configBytes, err := io.ReadAll(configFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully read config.json")
+
+	config := []Route{}
+	if err := json.Unmarshal(configBytes, &config); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully loaded config.json")
+}
